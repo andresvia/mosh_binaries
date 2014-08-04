1329e7d340938261f10aae94d343bfd819a6669e
Andres Villarroel <andres.via@gmail.com>
2014-08-04 21:42:21 +0100

    adding ubuntu/precise64

---
+++ ubuntu/precise64/download.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -eux
+
+mirror=http://mirrors.kernel.org/ubuntu
+
+temp_dir=$(mktemp -d)
+
+trap "rm -rfv $temp_dir" EXIT
+
+pushd $temp_dir
+
+pkgs=(
+$mirror/pool/universe/m/mosh/mosh_1.1.3-1_amd64.deb
+$mirror/pool/main/p/protobuf/libprotobuf7_2.4.1-1ubuntu2_amd64.deb
+$mirror/pool/main/libu/libutempter/libutempter0_1.1.5-4_amd64.deb
+)
+
+bins=(
+usr/bin/mosh-server
+usr/lib/libprotobuf.so.7
+usr/lib/libutempter.so.0
+)
+
+for deb_pkg in ${pkgs[@]}
+do
+  wget $deb_pkg
+  ar xv $(basename "$deb_pkg")
+  tar xvzf data.tar.gz
+done
+
+popd
+
+for bin in ${bins[@]}
+do
+  rm -vf $bin
+  cp -Lvf $temp_dir/$bin .
+done
+
