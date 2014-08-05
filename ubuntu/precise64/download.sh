f79e9732f704124b500549d9e75a34b656f9f43a
Andres Villarroel <andres.via@gmail.com>
2014-08-05 08:28:40 +0100

    Replace mosh with backports version.

--- ubuntu/precise64/download.sh
+++ ubuntu/precise64/download.sh
@@ -9,7 +9,7 @@ trap "rm -rfv $temp_dir" EXIT
 pushd $temp_dir
 
 pkgs=(
-$mirror/pool/universe/m/mosh/mosh_1.1.3-1_amd64.deb
+$mirror/pool/universe/m/mosh/mosh_1.2.4a-1~ubuntu12.04.1_amd64.deb
 $mirror/pool/main/p/protobuf/libprotobuf7_2.4.1-1ubuntu2_amd64.deb
 $mirror/pool/main/libu/libutempter/libutempter0_1.1.5-4_amd64.deb
 )
